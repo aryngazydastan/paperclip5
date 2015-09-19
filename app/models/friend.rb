b4bc853a56622146d69d325a3d4ca880af1f698b
aryngazydastan <aryngazy.dastan@gmail.com>
2015-09-19 18:16:48 +0600

    Undo model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,9 @@
 class Friend < ActiveRecord::Base
   validates :name, presence: true
   do_not_validate_attachment_file_type :avatar
-  
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 end
